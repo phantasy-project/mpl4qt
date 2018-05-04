f2ac63eaea6200ad48a31fb96a751599db644db0
Tong Zhang <zhangt@frib.msu.edu>
2018-05-04 15:11:04 -0400

    NEW: Initial setup.py.

---
+++ setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+def read_license():
+    with open('LICENSE') as f:
+        return f.read()
+
+app_name = "mpl4qt"
+app_description = 'Matplotlib widget for PyQt'
+app_long_description = readme() + '\n\n'
+app_platform = ["Linux"]
+app_author = "Tong Zhang"
+app_author_email = "zhangt@frib.msu.edu"
+app_license = read_license()
+app_url = "https://archman.github.io/mpl4qt/"
+app_keywords = "widgets Qt designer PyQt matplotlib"
+installrequires = [
+    'matplotlib',
+]
+
+setup(
+    name=app_name,
+    version="0.1.0",
+    description=app_description,
+    long_description=app_long_description,
+    author=app_author,
+    author_email=app_author_email,
+    url=app_url,
+    platforms=app_platform,
+    license=app_license,
+    keywords=app_keywords,
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules', 
+    ],
+    #install_requires=installrequires,
+)
