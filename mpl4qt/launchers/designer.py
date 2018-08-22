3a0f5f383de823f8c51c1f2f12eab2d00a402389
Tong Zhang <zhangt@frib.msu.edu>
2018-08-22 10:37:53 -0400

    ENH: Add launcher for qt-designer.

---
+++ mpl4qt/launchers/designer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from PyQt5.QtCore import QProcessEnvironment
+from PyQt5.QtCore import QProcess
+from PyQt5.QtCore import QLibraryInfo
+
+curdir = os.path.dirname(__file__)
+
+def main():
+    env = QProcessEnvironment.systemEnvironment()
+    plugin_path = os.path.abspath(os.path.join(curdir, '../plugins'))
+    env.insert('PYQTDESIGNERPATH', plugin_path)
+
+    designer = QProcess()
+    designer.setProcessEnvironment(env)
+    designer_bin = QLibraryInfo.location(QLibraryInfo.BinariesPath)
+
+    designer.start('{}/designer'.format(designer_bin))
+    designer.waitForFinished(-1)
+
+    sys.exit(designer.exitCode())
