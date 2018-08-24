e1eebb95521cc8e6dc92b03b3fab2da312c9b86b
Tong Zhang <zhangt@frib.msu.edu>
2018-08-24 17:02:33 -0400

    ENH: Feature major slots for configuration widget.

--- mpl4qt/widgets/mplbasewidget.py
+++ mpl4qt/widgets/mplbasewidget.py
@@ -1,8 +1,32 @@
 # -*- coding: utf-8 -*-
 
+"""
+mplbasewidget.py
+
+Base class for matplotlib widget for PyQt.
+
+Copyright (C) 2018 Tong Zhang <zhangt@frib.msu.edu>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+"""
+
 from PyQt5.QtWidgets import QSizePolicy
 from PyQt5.QtGui import QPalette
 from PyQt5.QtWidgets import QWidget
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFontDatabase
 
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
@@ -16,6 +40,7 @@ MPL_VERSION = mpl.__version__
 
 
 class BasePlotWidget(FigureCanvas):
+
     def __init__(self, parent=None, width=5, height=4, dpi=100, **kws):
         self.figure = Figure(figsize=(width, height), dpi=dpi)
         self.axes = self.figure.add_subplot(111)
@@ -24,8 +49,16 @@ class BasePlotWidget(FigureCanvas):
         self.setParent(parent)
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.sys_bg_color = self.palette().color(QPalette.Background)
+        DEFAULT_FONTS = {
+            'title': QFontDatabase.systemFont(QFontDatabase.TitleFont),
+            'fixed': QFontDatabase.systemFont(QFontDatabase.FixedFont),
+            'general': QFontDatabase.systemFont(QFontDatabase.GeneralFont),
+        }
+        self.sys_label_font = DEFAULT_FONTS['general']
+        self.sys_title_font = DEFAULT_FONTS['title']
         self.post_style_figure()
         self.adjustSize()
+        self.set_context_menu()
 
     def post_style_figure(self):
         self.set_figure_color()
@@ -47,8 +80,61 @@ class BasePlotWidget(FigureCanvas):
         else:
             self.axes.set_axis_bgcolor(color)
 
+    def set_ticks_color(self, color=None):
+        if color is None:
+            color = self.sys_bg_color.getRgbF()
+        all_lbls = self.axes.get_xticklabels() + self.axes.get_yticklabels()
+        [lbl.set_color(color) for lbl in all_lbls]
+
+    def toggle_mticks(self, f):
+        if f:
+            self.axes.minorticks_on()
+        else:
+            self.axes.minorticks_off()
+
+    def toggle_grid(self, toggle_checked=False, which='major', b=None, color=None, **kws):
+        if toggle_checked:
+            which = 'both' if kws.get('mticks', True) else 'major'
+            self.axes.grid(which=which, color=color, linestyle='--')
+        else:
+            self.axes.grid(b=False, which='minor')
+            self.axes.grid(b=False)
+
+    def set_xylabel_font(self, font=None):
+        if font is None:
+            font = self.sys_label_font
+        _set_font(self.axes.xaxis.label, font)
+        _set_font(self.axes.yaxis.label, font)
+
+    def set_xyticks_font(self, font=None):
+        if font is None:
+            font = self.sys_label_font
+        all_lbls = self.axes.get_xticklabels() + self.axes.get_yticklabels()
+        [_set_font(lbl, font) for lbl in all_lbls]
+
+    def set_title_font(self, font=None):
+        if font is None:
+            font = self.sys_title_font
+        _set_font(self.axes.title, font)
+
     def update_canvas(self):
         self.draw_idle()
+
+    def set_context_menu(self,):
+        self.setContextMenuPolicy(Qt.DefaultContextMenu)
+
+
+def _set_font(obj, font):
+   obj.set_size(font.pointSizeF())
+   obj.set_family(font.family())
+   obj.set_weight(int(font.weight()/87.0*1000))
+   obj.set_stretch(font.stretch())
+   if font.italic():
+       obj.set_style('italic')
+   else:
+       obj.set_style('normal')
+
+
 
 #class BasePlotWidget(QWidget):
 #    """Base class for figure plot panel.
--- mpl4qt/widgets/mplcurvewidget.py
+++ mpl4qt/widgets/mplcurvewidget.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """
 mplcurvewidget.py
@@ -22,14 +23,22 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
 
-from PyQt5.QtCore import pyqtProperty, pyqtSlot, QSize
+from PyQt5.QtCore import pyqtProperty
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import QSize
 from PyQt5.QtCore import QVariant
 from PyQt5.QtGui import QColor
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QMenu
+from PyQt5.QtWidgets import QAction
+from PyQt5.QtWidgets import QDialog
+from PyQt5.QtGui import QFont
 
 import numpy as np
 
 from mpl4qt.widgets.mplbasewidget import BasePlotWidget
+from mpl4qt.widgets.mplconfig import MatplotlibConfigPanel
 
 
 class MatplotlibCurveWidget(BasePlotWidget):
@@ -39,25 +48,30 @@ class MatplotlibCurveWidget(BasePlotWidget):
     and slots that can be used to customize its appearance.
     """
     def __init__(self, parent=None):
-        self._fig_width = 5
-        self._fig_height = 4
-        self._fig_dpi = 100
+        self._fig_width = 4
+        self._fig_height = 3
+        self._fig_dpi = 75
         self._fig_tight_layout = False
         self._fig_auto_scale = False
+        self._fig_mticks_toggle = False
+        self._fig_grid_toggle = False
         super(MatplotlibCurveWidget, self).__init__(parent,
                 self._fig_width, self._fig_height, self._fig_dpi)
         self._fig_bgcolor = self.sys_bg_color
-
-        # test
-        # 1.5.1
-        #self.axes.set_axis_bgcolor("blue")
-        # 2.1.2
-        #self.axes.set_facecolor("blue")
+        self._fig_ticks_color = self.sys_bg_color
+        self._fig_grid_color = QColor('gray')
+        self._fig_title = ''
+        self._fig_xlabel = ''
+        self._fig_ylabel = ''
+        self._fig_xylabel_font = self.sys_label_font
+        self._fig_xyticks_font = self.sys_label_font
+        self._fig_title_font = self.sys_title_font
 
     def init_figure(self):
-        x = np.linspace(-4, 4, 400)
-        y = np.sin(10 * x) / x
-        self.line, = self.axes.plot(x, y, 'r-')
+        # initial xy data and line
+        self._x_data = x = np.linspace(-4, 4, 100)
+        self._y_data = y = np.sin(10 * x) / x
+        self._line, = self.axes.plot(x, y, 'r-')
 
     def sizeHint(self):
         return QSize(1.1 * self._fig_width * self._fig_dpi,
@@ -78,13 +92,49 @@ class MatplotlibCurveWidget(BasePlotWidget):
     figureTightLayout = pyqtProperty(bool, getTightLayoutToggle,
             setTightLayoutToggle)
 
+    def getFigureMTicksToggle(self):
+        return self._fig_mticks_toggle
+
+    @pyqtSlot(bool)
+    def setFigureMTicksToggle(self, f):
+        self._fig_mticks_toggle = f
+        self.toggle_mticks(f)
+        self.update_figure()
+
+    figureMTicksToggle = pyqtProperty(bool, getFigureMTicksToggle,
+            setFigureMTicksToggle)
+
+    def getFigureGridToggle(self):
+        return self._fig_grid_toggle
+
+    @pyqtSlot(bool)
+    def setFigureGridToggle(self, f, **kws):
+        self._fig_grid_toggle = f
+        self.toggle_grid(f, color=self._fig_grid_color.getRgbF(), **kws)
+        self.update_figure()
+
+    figureGridToggle = pyqtProperty(bool, getFigureGridToggle,
+            setFigureGridToggle)
+
+    def getFigureGridColor(self):
+        return self._fig_grid_color
+
+    @pyqtSlot(QColor)
+    def setFigureGridColor(self, c, **kws):
+        self._fig_grid_color = c
+        self.toggle_grid(color=c.getRgbF(), **kws)
+        self.update_figure()
+
+    figureGridColor = pyqtProperty(QColor, getFigureGridColor,
+            setFigureGridColor)
+
     def getFigureWidth(self):
         return self._fig_width
 
     @pyqtSlot(int)
     def setFigureWidth(self, w):
         self._fig_width = max(w, 2)
-        self.figure.set_size_inches([w, self._fig_height])
+        self.figure.set_size_inches([self._fig_width, self._fig_height])
         self.update_figure()
 
     figureWidth = pyqtProperty(int, getFigureWidth, setFigureWidth)
@@ -95,7 +145,7 @@ class MatplotlibCurveWidget(BasePlotWidget):
     @pyqtSlot(int)
     def setFigureHeight(self, h):
         self._fig_height = max(h, 2)
-        self.figure.set_size_inches([self._fig_width, h])
+        self.figure.set_size_inches([self._fig_width, self._fig_height])
         self.update_figure()
 
     figureHeight = pyqtProperty(int, getFigureHeight, setFigureHeight)
@@ -111,6 +161,42 @@ class MatplotlibCurveWidget(BasePlotWidget):
 
     figureDPI = pyqtProperty(int, getFigureDpi, setFigureDpi)
 
+    def getFigureXYlabelFont(self):
+        return self._fig_xylabel_font
+
+    @pyqtSlot(QFont)
+    def setFigureXYlabelFont(self, font):
+        self._fig_xylabel_font = font
+        self.set_xylabel_font(font)
+        self.update_figure()
+
+    figureXYlabelFont = pyqtProperty(QFont, getFigureXYlabelFont,
+            setFigureXYlabelFont)
+
+    def getFigureXYticksFont(self):
+        return self._fig_xyticks_font
+
+    @pyqtSlot(QFont)
+    def setFigureXYticksFont(self, font):
+        self._fig_xyticks_font = font
+        self.set_xyticks_font(font)
+        self.update_figure()
+
+    figureXYticksFont = pyqtProperty(QFont, getFigureXYticksFont,
+            setFigureXYticksFont)
+
+    def getFigureTitleFont(self):
+        return self._fig_title_font
+
+    @pyqtSlot(QFont)
+    def setFigureTitleFont(self, font):
+        self._fig_title_font = font
+        self.set_title_font(font)
+        self.update_figure()
+
+    figureTitleFont = pyqtProperty(QFont, getFigureTitleFont,
+            setFigureTitleFont)
+
     def getFigureBgColor(self):
         return self._fig_bgcolor
 
@@ -123,19 +209,17 @@ class MatplotlibCurveWidget(BasePlotWidget):
     figureBackgroundColor = pyqtProperty(QColor, getFigureBgColor,
             setFigureBgColor)
 
-    def getCurveXData(self):
-        return self.line.get_xdata().__len__()
-    
-    @pyqtSlot(int)
-    def setCurveXData(self, n):
-        xx = np.random.normal(0, 10, n)
-        hist, edge = np.histogram(xx, bins=100, density=True)
-        self.line.set_xdata((edge[:-1]+edge[1:])/2.0)
-        self.line.set_ydata(hist)
-        self.update_canvas()
+    def getFigureXYticksColor(self):
+        return self._fig_ticks_color
+
+    @pyqtSlot(QColor)
+    def setFigureXYticksColor(self, color):
+        self._fig_ticks_color = color
+        self.set_ticks_color(color.getRgbF())
         self.update_figure()
 
-    curveXData = pyqtProperty(int, getCurveXData, setCurveXData)
+    figureXYticksColor = pyqtProperty(QColor, getFigureXYticksColor,
+            setFigureXYticksColor)
 
     def getFigureAutoScale(self):
         return self._fig_auto_scale
@@ -150,12 +234,91 @@ class MatplotlibCurveWidget(BasePlotWidget):
 
     figureAutoScale = pyqtProperty(bool, getFigureAutoScale, setFigureAutoScale)
 
+    def getFigureTitle(self):
+        return self._fig_title
+
+    @pyqtSlot('QString')
+    def setFigureTitle(self, s):
+        self._fig_title = s
+        self.axes.set_title(s)
+        self.update_figure()
+
+    figureTitle = pyqtProperty('QString', getFigureTitle, setFigureTitle)
+
+    def getFigureXlabel(self):
+        return self._fig_xlabel
+
+    @pyqtSlot('QString')
+    def setFigureXlabel(self, s):
+        self._fig_xlabel = s
+        self.axes.set_xlabel(s)
+        self.update_figure()
+
+    figureXlabel = pyqtProperty('QString', getFigureXlabel, setFigureXlabel)
+
+    def getFigureYlabel(self):
+        return self._fig_ylabel
+
+    @pyqtSlot('QString')
+    def setFigureYlabel(self, s):
+        self._fig_ylabel = s
+        self.axes.set_ylabel(s)
+        self.update_figure()
+
+    figureYlabel = pyqtProperty('QString', getFigureYlabel, setFigureYlabel)
+
+    @pyqtSlot(QVariant)
+    def setXData(self, x):
+        """set x data for figure."""
+        self._x_data = x
+        self._line.set_xdata(x)
+        self.update_figure()
+
+    def getXData(self):
+        return self._x_data
+
+    @pyqtSlot(QVariant)
+    def setYData(self, x):
+        """set y data for figure."""
+        self._y_data = x
+        self._line.set_ydata(x)
+        self.update_figure()
+
+    def getYData(self):
+        return self._y_data
+
     def update_figure(self):
         if self.getFigureAutoScale():
             self.axes.relim()
             self.axes.autoscale()
         self.figure.canvas.draw_idle()
         self.update()
+
+    def contextMenuEvent(self, e):
+        menu = QMenu(self)
+        config_action = QAction("Config", menu)
+        reset_action = QAction("Reset", menu)
+        menu.addAction(config_action)
+        menu.addSeparator()
+        menu.addAction(reset_action)
+
+        config_action.triggered.connect(self.on_config)
+        reset_action.triggered.connect(self.on_reset)
+
+        menu.exec_(self.mapToGlobal(e.pos()))
+
+        #menu.move(e.globalPos())
+        #menu.show()
+        #menu.activateWindow()
+
+    def on_config(self):
+        config_panel = MatplotlibConfigPanel(self)
+        r = config_panel.exec_()
+        #print(r == QDialog.Accepted)
+        #print(r == QDialog.Rejected)
+
+    def on_reset(self):
+        print("Reset action triggered")
 
 
 if __name__ == "__main__":
