561ac991cda07efa8a58db1cb142ffc30245dda0
Tong Zhang <zhangt@frib.msu.edu>
2018-08-28 16:00:14 -0400

    MNT: Add project meta information.

---
+++ mpl4qt/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__authors__ = "Tong Zhang"
+__copyright__ = "(c) 2018, Facility for Rare Isotope beams," \
+                " Michigan State University"
+__contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
+__version__ = "1.0.0"
