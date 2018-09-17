428fbaf643ec8ac7428cf9ad17298fd8fc612777
Tong Zhang <zhangt@frib.msu.edu>
2018-09-17 14:30:35 -0400

    FIX: Compatibility issue mpl 1.x with 2.x.

--- mpl4qt/__init__.py
+++ mpl4qt/__init__.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
+
+# always use Qt5Agg for mpl < 2.0
+import matplotlib
+if matplotlib.__version__ < "2.0.0":
+    matplotlib.use("Qt5Agg")
+#
+
 __authors__ = "Tong Zhang"
 __copyright__ = "(c) 2018, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
 __version__ = "1.3.1"
-
-import matplotlib
-matplotlib.use("Qt5Agg")
