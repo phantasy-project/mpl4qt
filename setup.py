e3f4be9259c39428e14282f68aa1d2b9f0b91965
Tong Zhang <zhangt@frib.msu.edu>
2018-09-26 14:57:39 -0400

    MNT: Bump up version to 2.0.0.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ installrequires = [
 
 setup(
     name=app_name,
-    version="1.4.0",
+    version="2.0.0",
     description=app_description,
     long_description=app_long_description,
     author=app_author,
