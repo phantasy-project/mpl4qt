ad50400742d211948068143bdc7253a4b084a21a
Tong Zhang <zhangt@frib.msu.edu>
2018-10-23 16:27:30 -0400

    ENH: Add new tool: lasso tool.

--- mpl4qt/widgets/mpltoolbar.py
+++ mpl4qt/widgets/mpltoolbar.py
@@ -5,16 +5,20 @@ Navigation toolbar for matplotlib widgets
 """
 
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as Toolbar
+from matplotlib.path import Path
+from matplotlib.widgets import LassoSelector
+import numpy as np
 
 from PyQt5.QtWidgets import QDialog, QPushButton
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtWidgets import QHBoxLayout
 from PyQt5.QtCore import QPoint
-from PyQt5.QtCore import Qt, pyqtSlot
+from PyQt5.QtCore import Qt, pyqtSlot, pyqtSignal, QVariant, QObject
 from mpl4qt.icons import exit_tool_icon
 from mpl4qt.icons import home_tool_icon
 from mpl4qt.icons import zoom_tool_icon
 from mpl4qt.icons import save_tool_icon
+from mpl4qt.icons import lasso_tool_icon
 
 
 class NavigationToolbar(Toolbar):
@@ -23,6 +27,7 @@ class NavigationToolbar(Toolbar):
 
 
 class ToolbarDialog(QDialog):
+
     def __init__(self, canvas, parent=None):
         super(ToolbarDialog, self).__init__()
         self.parent = parent
@@ -68,11 +73,19 @@ class ToolbarDialog(QDialog):
         save_btn.setIcon(save_icon)
         save_btn.setToolTip("Save figure as file")
 
+        # lasso tool
+        lasso_btn = QPushButton("", self)
+        lasso_icon = QIcon(QPixmap(lasso_tool_icon))
+        lasso_btn.setIcon(lasso_icon)
+        lasso_btn.setCheckable(True)
+        lasso_btn.setToolTip("Select point(s) by lasso")
+
         # layout
         hbox = QHBoxLayout()
         hbox.addWidget(tb)
         hbox.addWidget(home_btn)
         hbox.addWidget(zoom_btn)
+        hbox.addWidget(lasso_btn)
         hbox.addWidget(save_btn)
         hbox.addWidget(exit_btn)
         self.setLayout(hbox)
@@ -80,6 +93,7 @@ class ToolbarDialog(QDialog):
         # events
         home_btn.clicked.connect(self.home)
         zoom_btn.toggled.connect(self.zoom)
+        lasso_btn.clicked.connect(self.lasso)
         exit_btn.clicked.connect(self.close)
         save_btn.clicked.connect(self.save)
 
@@ -95,6 +109,23 @@ class ToolbarDialog(QDialog):
     def save(self):
         self.tb.save_figure()
 
+    @pyqtSlot()
+    def lasso(self):
+        if self.sender().isChecked():
+            _x = self.parent.getXData()
+            _y = self.parent.getYData()
+            pts = np.vstack([_x, _y]).T
+            ax = self.parent.axes
+            self.selector = SelectFromPoints(ax, pts)
+            self.selector.selectedIndicesReady.connect(self.show_selected_indices)
+        else:
+            self.selector.disconnect()
+            self.selector.selectedIndicesReady.disconnect()
+
+    @pyqtSlot(QVariant)
+    def show_selected_indices(self, x):
+        print(x)
+
     def closeEvent(self, e):
         if self.zoom_btn.isChecked():
             self.zoom_btn.setChecked(False) # emit toggled
@@ -108,3 +139,28 @@ class ToolbarDialog(QDialog):
                 self.parent.geometry().width() - self.geometry().width())
         y = self.parent.geometry().y()
         return self.parent.mapToGlobal(QPoint(x, y))
+
+
+class SelectFromPoints(QObject):
+    """Select indices from points using `LassoSelector`.
+    """
+    selectedIndicesReady = pyqtSignal(QVariant)
+    def __init__(self, ax, points, alpha_other=0.3, radius=0):
+        super(SelectFromPoints, self).__init__()
+        self.canvas = ax.figure.canvas
+        self.points = points
+        self.alpha_other = alpha_other
+        self.radius = radius
+
+        self.lasso = LassoSelector(ax, onselect=self.on_select)
+
+    def on_select(self, verts):
+        path = Path(verts)
+        ind = np.nonzero(
+                path.contains_points(self.points, radius=self.radius))[0]
+        self.canvas.draw_idle()
+        self.selectedIndicesReady.emit(ind)
+
+    def disconnect(self):
+        self.lasso.disconnect_events()
+        self.canvas.draw_idle()
