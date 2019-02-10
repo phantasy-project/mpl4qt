b42fff4da76971dd294ea1c1366860e16a3806a6
Tong Zhang <zhangt@frib.msu.edu>
2019-02-10 10:48:27 -0500

    MNT: Bump up version to 2.3.2.

--- mpl4qt/__init__.py
+++ mpl4qt/__init__.py
@@ -11,4 +11,4 @@ __authors__ = "Tong Zhang"
 __copyright__ = "(c) 2018, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
-__version__ = "2.3.1"
+__version__ = "2.3.2"
