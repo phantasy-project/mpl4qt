408c915164d14bed93d71d37e8d9b0a3dbb3cb20
Tong Zhang <zhangt@frib.msu.edu>
2019-03-01 14:27:15 -0500

    MNT: Bump up version to 2.3.3.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ installrequires = [
 
 setup(
     name=app_name,
-    version="2.3.2",
+    version="2.3.3",
     description=app_description,
     long_description=app_long_description,
     author=app_author,
