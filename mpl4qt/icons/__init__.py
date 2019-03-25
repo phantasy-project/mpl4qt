e58ce903cbb94d9ba266c3b34b947f4d7e8bc2ac
Tong Zhang <zhangt@frib.msu.edu>
2019-03-25 14:26:52 -0400

    ENH: Add icons for dock/undock tool.

--- mpl4qt/icons/__init__.py
+++ mpl4qt/icons/__init__.py
@@ -5,7 +5,6 @@ from ._export import icon as export_icon
 from .mplcurvewidget_icon import icon as mplcurvewidget_icon
 from .mplbasewidget_icon import icon as mplbasewidget_icon
 from .mplerrorbarwidget_icon import icon as mplerrorbarwidget_icon
-# to be replaced
 from .mplimagewidget_icon import icon as mplimagewidget_icon
 from .mplbarwidget_icon import icon as mplbarwidget_icon
 from ._home import icon as home_tool_icon
@@ -16,4 +15,6 @@ from ._save import icon as save_tool_icon
 from ._lasso import icon as lasso_tool_icon
 from ._repos import icon as repos_tool_icon
 from ._cross import icon as cross_tool_icon
+from ._dock import icon as dock_tool_icon
+from ._popup import icon as popup_tool_icon
 from ._tools import icon as tools_icon
