e1489fdd9c3b428ff0241e1531e9fe8aebc4466f
Tong Zhang <zhangt@frib.msu.edu>
2019-04-03 09:51:09 -0400

    FIX: Conditional config matplotlib for Debian.

--- mpl4qt/__init__.py
+++ mpl4qt/__init__.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import platform
+import matplotlib
+
+d, v, _ = platform.linux_distribution()
+if d == 'debian':
+    matplotlib.rcParams['agg.path.chunksize'] = 2000
 
 # always use Qt5Agg for mpl < 2.0
-import matplotlib
 if matplotlib.__version__ <= "2.0.0":
     matplotlib.use("Qt5Agg")
-    matplotlib.rcParams['agg.path.chunksize'] = 2000
 #
 
 __authors__ = "Tong Zhang"
