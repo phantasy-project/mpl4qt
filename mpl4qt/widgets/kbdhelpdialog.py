3e94f5d04c3b66797309d64036c48eedb9fcaed1
Tong Zhang <zhangt@frib.msu.edu>
2020-05-21 13:50:29 -0400

    ENH: Update keyshort help window and fix style on windows OS.

--- mpl4qt/widgets/kbdhelpdialog.py
+++ mpl4qt/widgets/kbdhelpdialog.py
@@ -3,6 +3,8 @@
 """Help dialog pop up thru '?'.
 """
 
+import platform
+
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter
 from PyQt5.QtGui import QPen
@@ -19,14 +21,17 @@ class KbdHelpDialog(QDialog, Ui_Dialog):
 
         # UI
         self.setupUi(self)
-        self.setWindowFlags(self.windowFlags() | Qt.FramelessWindowHint)
-        self.setAttribute(Qt.WA_TranslucentBackground)
+        # self.setWindowFlags(self.windowFlags() | Qt.FramelessWindowHint)
+        if platform.system() == "Linux":
+            self.setAttribute(Qt.WA_TranslucentBackground)
+        else:
+            self.setWindowOpacity(0.8)
+            self.setStyleSheet("QDialog{background:#000000;}")
         self.adjustSize()
 
     def paintEvent(self, e):
         painter = QPainter(self)
-        painter.setOpacity(0.75)
+        painter.setOpacity(0.8)
         painter.setBrush(Qt.white)
         painter.setPen(QPen(Qt.white))
         painter.drawRect(self.rect())
-
