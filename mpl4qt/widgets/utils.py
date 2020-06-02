922a70a9f71257e02f9e7c5a7697d2dd474140de
Tong Zhang <zhangt@frib.msu.edu>
2020-06-01 21:49:56 -0400

    ENH: Add color cycle constant object.

--- mpl4qt/widgets/utils.py
+++ mpl4qt/widgets/utils.py
@@ -4,6 +4,7 @@ import json
 import re
 from collections import OrderedDict
 from copy import deepcopy
+from itertools import cycle
 
 import matplotlib.colors as colors
 import numpy as np
@@ -194,6 +195,11 @@ COLORMAPS_DICT = OrderedDict(COLORMAPS)
 ALL_COLORMAPS = []
 for k, v in COLORMAPS:
     ALL_COLORMAPS.extend(v)
+
+COLOR_CYCLE = cycle(['#1f77b4', '#ff7f0e', '#2ca02c',
+                     '#d62728', '#9467bd', '#8c564b',
+                     '#e377c2', '#7f7f7f', '#bcbd22',
+                     '#17becf'])
 
 # font style
 FONT_STYLE_Q2M = {
