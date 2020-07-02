4cbb7de6d34bb61e25d0045909bf8dbe6d5c26e0
Tong Zhang <zhangt@frib.msu.edu>
2020-07-02 13:37:44 -0400

    MNT: Update package info.

--- mpl4qt/widgets/_info.py
+++ mpl4qt/widgets/_info.py
@@ -7,10 +7,10 @@ def get_pkg_info():
     info = """
     <html>
     <h4>About mpl4qt</h4>
-    <p>Data visualization widgets for Qt and designer, developed with
-    <a href="https://matplotlib.org">matplotlib</a>, current version is {}.
+    <p>Data visualization widgets based on
+    <a href="https://matplotlib.org">matplotlib</a> for PyQt5 applications, current version is {v}.
     </p>
-    <p>Copyright (C) 2018 Tong Zhang</p>
+    <p>Copyright (C) 2018-2020 <a href="mailto:zhangt@frib.msu.edu?subject=Questions about mpl4qt v{v}">Tong Zhang</a></p>
     </html>
-    """.format(ver)
+    """.format(v=ver)
     return info
