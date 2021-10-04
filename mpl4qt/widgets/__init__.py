c13520fb040c6f73def42d9477bad3c37c56367e
Tong Zhang <zhangt@frib.msu.edu>
2021-10-04 16:21:22 -0400

    MNT: Add MatplotlibBarWidget into mpl4qt.widgets namespace.

--- mpl4qt/widgets/__init__.py
+++ mpl4qt/widgets/__init__.py
@@ -2,10 +2,12 @@ from .mplbasewidget import MatplotlibBaseWidget
 from .mplcurvewidget import MatplotlibCurveWidget
 from .mplerrorbarwidget import MatplotlibErrorbarWidget
 from .mplimagewidget import MatplotlibImageWidget
+from .mplbarwidget import MatplotlibBarWidget
 
 __all__ = [
     'MatplotlibBaseWidget',
     'MatplotlibCurveWidget',
     'MatplotlibErrorbarWidget',
     'MatplotlibImageWidget',
+    'MatplotlibBarWidget',
 ]
