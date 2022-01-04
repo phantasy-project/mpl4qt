3f323aca716b58f5d81b28895be66ea811dde410
Tong Zhang <zhangt@frib.msu.edu>
2022-01-04 10:14:26 -0500

    MNT: Bump version to 2.7.5.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ installrequires = [
 
 setup(
     name=app_name,
-    version="2.7.4",
+    version="2.7.5",
     description=app_description,
     long_description=app_long_description,
     long_description_content_type='text/markdown',
