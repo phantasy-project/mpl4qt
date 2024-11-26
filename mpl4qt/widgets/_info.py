42a0b32950c131fe7ae859a8956757de0442e2f8
Tong Zhang <zhangt@frib.msu.edu>
2024-11-26 10:02:15 -0500

    MNT: Updated About info, included version of matplotlib.

--- mpl4qt/widgets/_info.py
+++ mpl4qt/widgets/_info.py
@@ -3,16 +3,18 @@
 
 def get_pkg_info():
     import mpl4qt
+    import matplotlib
+    mpl_ver = matplotlib.__version__
     ver = mpl4qt.__version__
-    info = """
-    <html>
+    info = f"""<html>
     <h4>About mpl4qt</h4>
+    <p><b>v{ver}</b></p>
     <p>Data visualization widgets based on
-    <a href="https://matplotlib.org">matplotlib</a> for PyQt5 applications, current version is {v}.
+    <a href="https://matplotlib.org">matplotlib</a> (v{mpl_ver}) for PyQt5 application development.
     </p>
-    <p>Copyright (C) 2018-2020 <a href="mailto:zhangt@frib.msu.edu?subject=Questions about mpl4qt v{v}">Tong Zhang</a></p>
-    </html>
-    """.format(v=ver)
+    <p>Copyright (C) 2018-2024 Facility for Rare Isotope Beams, Michigan State University.</p>
+    <p>Contact: <a href="mailto:Tong%20Zhang<zhangt@frib.msu.edu>?subject=Questions about mpl4qt v{ver} with matplotlib v{mpl_ver}">Tong Zhang</a></p>
+    </html>"""
     return info
 
 
